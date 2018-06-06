44026f91d8e7e9dee1a78f6fa1e719346fbd173c
Abel Pinheiro de Figueiredo <abelpinheiro95@gmail.com>
2018-06-06 00:17:56 -0300

    Layout of the initial page layout of the first question (Science)

--- app/src/main/java/abelpinheiro/github/io/generalknowledge/MainActivity.java
+++ app/src/main/java/abelpinheiro/github/io/generalknowledge/MainActivity.java
@@ -2,6 +2,8 @@ package abelpinheiro.github.io.generalknowledge;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void scienceButtonPressed(View view){
+        setContentView(R.layout.question_one);
+    }
+
+    public void nextButtonPressed(View view){
+        //TODA VEZ QUE FOR PRESSIONADO, ELE IRÁ VERIFICAR O ATRIBUTO QUESTAOATUAL PARA VERIFICAR EM QUAL QUESTÃO SE ENCONTRA. A PARTIR DISSO ELE VAI SELECIONAR POR UM IF
+        //QUAL LAYOUT IRÁ FAZER O SETCONTENTVIEW
     }
 }
